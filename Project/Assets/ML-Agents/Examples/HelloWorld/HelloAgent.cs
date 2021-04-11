9a518f81aeb09bb347517fa175c5df3f3a5dd2cc
Ben Palevsky <bpalevsky@gmail.com>
2021-04-11 16:10:50 -0400

    Get agent running using heuristics

---
+++ Project/Assets/ML-Agents/Examples/HelloWorld/HelloAgent.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class HelloAgent : Agent
+{
+    public Transform player;
+    public Transform goal;
+    public Transform death;
+
+    public override void CollectObservations(VectorSensor sensor)
+    {        
+        base.CollectObservations(sensor);
+        sensor.AddObservation(player.transform.position.x);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        base.Heuristic(actionsOut);
+        var actions = actionsOut.ContinuousActions;
+        actions[0] = Input.GetKey(KeyCode.RightArrow) ? 1f : Input.GetKey(KeyCode.LeftArrow) ? -1f : 0;        
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+        Move(actions.ContinuousActions[0]);
+    }
+
+    void Move(float delta) {
+        player.position += new Vector3(delta, 0f, 0f);
+    }
+
+    void Restart() {
+        player.position = Vector3.zero;
+    }
+
+    void Update() {
+        if (hasWon) {
+            AddReward(1f);
+            EndEpisode();
+            Restart();
+        } else if (hasLost) {
+            AddReward(-1f);
+            EndEpisode();
+            Restart();
+        } else {
+            RequestDecision();
+        }
+    }
+
+    bool hasWon => player.position.x >= goal.position.x;
+    bool hasLost => player.position.x <= death.position.x;
+
+    public override void OnEpisodeBegin()
+    {
+        base.OnEpisodeBegin();
+    }
+
+}
